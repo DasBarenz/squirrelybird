d1a18e7164c7455522ab4d6c3817e695897bc3da
DasBarenz <42988309+DasBarenz@users.noreply.github.com>
2019-02-10 21:30:49 -0700

    everything works! needs styling.

--- server.js
+++ server.js
@@ -1,122 +1,93 @@
-// Using the tools and techniques you learned so far,
-// you will scrape a website of your choice, then place the data
-// in a MongoDB database. Be sure to make the database and collection
-// before running this exercise.
-
-// Consult the assignment files from earlier in class
-// if you need a refresher on Cheerio.
-
-// Dependencies
 var express = require("express");
-var mongojs = require("mongojs");
-// Require axios and cheerio. This makes the scraping possible
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
 var axios = require("axios");
 var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
+var PORT = 3000;
 
 // Initialize Express
 var app = express();
 
-// Database configuration
-var squirrelDB = "scraper";
-var collections = ["scrapedData"];
+// Configure middleware
 
-// Hook mongojs configuration to the db variable
-var db = mongojs(squirrelDB, collections);
-db.on("error", function (error) {
-  console.log("Database Error:", error);
-});
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
 
-// Main route (simple Hello World Message)
-app.get("/", function (req, res) {
-  res.send("Hello world");
-});
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/squirrelyBird", { useNewUrlParser: true });
 
-// Retrieve data from the db
-app.get("/all", function (req, res) {
-  // Find all results from the scrapedData collection in the db
-  db.scrapedData.find({}, function (error, found) {
-    // Throw any errors to the console
-    if (error) {
-      console.log(error);
-    }
-    // If there are no errors, send the data to the browser as json
-    else {
-      res.json(found);
-    }
-  });
-});
-
-//actual scraping works
-// axios.get("https://www.duffelblog.com/").then(function(response) {
-
-//   // Load the HTML into cheerio and save it to a variable
-//   // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
-//   var $ = cheerio.load(response.data);
-
-//   // An empty array to save the data that we'll scrape
-//   var results = [];
-
-//   // Select each element in the HTML body from which you want information.
-//   // NOTE: Cheerio selectors function similarly to jQuery's selectors,
-//   // but be sure to visit the package's npm page to see how it works
-//   $("li.mvp-blog-story-wrap").each(function(i, element) {
-
-//     var link = $(element).find("a").attr("href");
-//     var title = $(element).find("h2").text();
-
-//     // Save these results in an object that we'll push into the results array we defined earlier
-//     results.push({
-//       title: title,
-//       link: link
-//     });
-//   });
-
-//   // Log the results once you've looped through each of the elements found with cheerio
-//   console.log(results);
-// });
-
-
-
-// Scrape data from one site and place it into the mongodb db
 app.get("/scrape", function (req, res) {
+  // this scrapes, console logs in node and puts objects on /articles
   axios.get("https://www.duffelblog.com/").then(function (response) {
-    // Load the html body from axios into cheerio
     var $ = cheerio.load(response.data);
-  
+
     $("li.mvp-blog-story-wrap").each(function (i, element) {
+      var result = {};
 
-      var link = $(element).find("a").attr("href");
-      var title = $(element).find("h2").text();
+      result.title = $(this).find("h2").text();
+      result.link = $(this).find("a").attr("href");
 
-      // If this found element had both a title and a link
-      if (title && link) {
-        // Insert the data in the scrapedData db
-        db.scrapedData.insert({
-          title: title,
-          link: link
-        },
-          function (err, inserted) {
-            if (err) {
-              // Log the error if one is encountered during the query
-              console.log(err);
-            }
-            else {
-              // Otherwise, log the inserted data
-              console.log(inserted);
-            }
-          });
-      }
+      db.Article.create(result)
+        .then(function (dbArticle) {
+          console.log(dbArticle);
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
     });
-  });
 
-  // Send a "Scrape Complete" message to the browser
-  res.send("Scrape Complete");
+    res.send("Scrape Complete");
+  });
 });
 
+//this works! 
+app.get("/articles", function (req, res) {
+  db.Article.find({})
+    .then(function (dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
+});
 
-// Listen on port 3000
+//these routes below now also work!
+app.get("/articles/:id", function (req, res) {
+  db.Article.findOne({ _id: req.params.id })
+
+    .populate("note")
+    .then(function (dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
+});
+
+app.post("/articles/:id", function (req, res) {
+  db.Note.create(req.body)
+    .then(function (dbNote) {
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function (dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
+
+});
+
 app.listen(3000, function () {
   console.log("App running on port 3000!");
 });
-
-
